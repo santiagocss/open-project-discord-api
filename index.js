ab2ae9a9d19af48f7ed1e08d7dc325319ee03acf
samet <36582849+santiagocss@users.noreply.github.com>
2023-03-01 03:46:17 +0300

    hot fix v1.06
    Fixed some bugs in the code and stabilized the badge feature.

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express();
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: 3276799 });
 
+const PAGE_SIZE = 300;
+
 app.get('/guilds/:guildid', (req, res) => {
   const guildId = req.params.guildid;
   const guild = client.guilds.cache.get(guildId);
-  if (!guild) return res.status(404).send({ message: 'Sunucu bulunamadı.' });
+  if (!guild) return res.status(404).send({ message: 'server not found.' });
 
   const rolesCount = guild.roles.cache.size;
   const emojisCount = guild.emojis.cache.size;
@@ -32,6 +34,36 @@ app.get('/guilds/:guildid', (req, res) => {
   res.send(response);
 });
 
-client.login('YOUR_BOT_TOKEN');
-app.listen(8080, () => console.log('Sunucu başlatıldı.'));
+
+
+app.get('/user/:userid', (req, res) => {
+    const userId = req.params.userid;
+    const user = client.users.cache.get(userId);
+    if (!user) return res.status(404).send({ message: 'user not found.' });
+  
+    const badges = user.flags?.toArray() || [];
+    const avatar = user.displayAvatarURL({ format: 'png', size: 1024 });
+    const banner = user.bannerURL({ format: 'png', size: 1024 });
+    const createdAt = user.createdAt.toISOString();
+  
+    const response = {
+      id: user.id,
+      username: user.username,
+      discriminator: user.discriminator,
+      badges,
+      avatar,
+      banner,
+      createdAt,
+    };
+  
+    res.send(response);
+  });
+app.listen(3000, () => {
+
+    console.log('Server is running on port 3000');
+}
+
+);
+
+client.login('MTA3NjExMzIxOTE4NzM4ODQ4OA.GHuYJs.kjNXcyZheL5f-70aKUjZ0OP04s2O4XG2O1FuCY');
 
